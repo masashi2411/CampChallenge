8dc3dd66385718569f6b049270ff7ffd529c9769
masashi2411 <isibayim1112@gmail.com>
2016-09-23 17:57:05 +0900

    no message

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/四則演算.php
@@ -0,0 +1,10 @@
+<?php
+
+    $tasi = 5 + 5;
+    $hiki = 15 - 5;   
+    $kake = 2 * 5;    
+    $wari = 20 / 2;    
+    $amari = 65 % 11;
+
+
+echo $tasi,$hiki,$kake,$wari,$amari;
---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/変数宣言、表示.php
@@ -0,0 +1,13 @@
+<?php
+
+ $x = 1;
+ 
+   if ($x == 1) {
+   	echo "1です！";
+   }  elseif ($x == 2) {
+   	echo "プログラミングキャンプ！";
+   } elseif ($x == a) {
+   	echo "文字です！";
+   } else {
+   	echo "その他です!";
+   }
